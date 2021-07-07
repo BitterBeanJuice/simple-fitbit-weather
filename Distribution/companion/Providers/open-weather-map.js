248bedb756dfafe67e412e8c003dd1a737b65027
BitterBeanJuice <82174579+BitterBeanJuice@users.noreply.github.com>
2021-07-06 21:37:15 -0700

    Update open-weather-map.js

--- Distribution/companion/Providers/open-weather-map.js
+++ Distribution/companion/Providers/open-weather-map.js
@@ -1,49 +1,10 @@
 import { Conditions } from "../../common";
 var mapping_codes = {
-    200: Conditions.Thunderstorm,
-    201: Conditions.Thunderstorm,
-    202: Conditions.Thunderstorm,
-    210: Conditions.Thunderstorm,
-    211: Conditions.Thunderstorm,
-    212: Conditions.Thunderstorm,
-    221: Conditions.Thunderstorm,
-    230: Conditions.Thunderstorm,
-    231: Conditions.Thunderstorm,
-    232: Conditions.Thunderstorm,
-    300: Conditions.Snow,
-    301: Conditions.Snow,
-    302: Conditions.Snow,
-    310: Conditions.Snow,
-    311: Conditions.Snow,
-    312: Conditions.Snow,
-    313: Conditions.Snow,
-    314: Conditions.Snow,
-    321: Conditions.Snow,
-    500: Conditions.Rain,
-    501: Conditions.Rain,
-    502: Conditions.Rain,
-    503: Conditions.Rain,
-    504: Conditions.Rain,
-    511: Conditions.Rain,
+    511: Conditions.Snow,
     520: Conditions.ShowerRain,
     521: Conditions.ShowerRain,
     522: Conditions.ShowerRain,
     531: Conditions.ShowerRain,
-    600: Conditions.Snow,
-    601: Conditions.Snow,
-    602: Conditions.Snow,
-    611: Conditions.Snow,
-    612: Conditions.Snow,
-    615: Conditions.Snow,
-    616: Conditions.Snow,
-    620: Conditions.Snow,
-    621: Conditions.Snow,
-    622: Conditions.Snow,
-    701: Conditions.Mist,
-    711: Conditions.Mist,
-    721: Conditions.Mist,
-    731: Conditions.Mist,
-    741: Conditions.Mist,
     800: Conditions.ClearSky,
     801: Conditions.FewClouds,
     802: Conditions.ScatteredClouds,
@@ -52,6 +13,27 @@ var mapping_codes = {
 };
 var tempToC = function (value) { return value - 273.15; };
 var tempToF = function (value) { return ((value - 273.15) * 9) / 5 + 32; };
+var getCondition = function (value) {
+    if (mapping_codes[value]) {
+        return mapping_codes[value];
+    }
+    if (value >= 200 && value < 300) {
+        return Conditions.Thunderstorm;
+    }
+    if (value >= 300 && value < 400) {
+        return Conditions.Rain;
+    }
+    if (value >= 500 && value < 600) {
+        return Conditions.Rain;
+    }
+    if (value >= 600 && value < 700) {
+        return Conditions.Snow;
+    }
+    if (value >= 700 && value < 800) {
+        return Conditions.Mist;
+    }
+    return Conditions.Unknown;
+};
 /**
  * Fetch data from Open Weather Map
  */
@@ -81,7 +63,7 @@ export function fetchWeather(apiKey, latitude, longitude) {
                         temperatureC: tempToC(d.temp.max),
                         temperatureF: tempToF(d.temp.max),
                         timestamp: d.dt,
-                        conditionCode: mapping_codes[condition] || Conditions.Unknown,
+                        conditionCode: getCondition(condition),
                         realConditionCode: condition.toString(),
                     };
                 }),
@@ -89,7 +71,7 @@ export function fetchWeather(apiKey, latitude, longitude) {
                     temperatureC: tempToC(data.current.temp),
                     temperatureF: tempToF(data.current.temp),
                     timestamp: data.current.dt,
-                    conditionCode: mapping_codes[currentCondition] || Conditions.Unknown,
+                    conditionCode: getCondition(currentCondition),
                     realConditionCode: currentCondition.toString(),
                 },
             };
